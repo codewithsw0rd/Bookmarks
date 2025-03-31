4e9c6655c15203db25fde351375cb7b71c1896b5
codewithsw0rd <thugkaustuv@gmail.com>
2025-03-31 11:06:57 +0545

    Added model to store custom user profile

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from .forms import LoginForm, UserRegisterationForm
+from .forms import LoginForm, UserRegisterationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -53,6 +54,8 @@ def register(request):
             )
             # Save the user object
             new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 'account/register_done.html',
@@ -64,4 +67,32 @@ def register(request):
         request,
         'account/register.html',
         {'user_form': user_form}
+    )
+    
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user,
+            data=request.POST   
+        )
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
     )
